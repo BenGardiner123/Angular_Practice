25dc20ec5d35b3576e08388f8211290146c97593
Ben Gardiner <bennygards@gmail.com>
2020-07-10 16:29:50 +1000

    done for the arvo

--- my-app/src/app/courses.component.ts
+++ my-app/src/app/courses.component.ts
@@ -6,13 +6,7 @@ import { CoursesService } from './courses.service';
     template: `                 
     <!-- ////using the the backtick here allows us to spread the tempalte over multiple lines -->
     <h2>{{ title }}</h2>
-    <ul>
-    <!-- when we use a directive like ngFor that manipulates the DOM by adding or removing an elemt- need to use an asterisk to annotate it -->
-        <li *ngFor="let course of courses">   
-              {{ course }}
-              <!-- "let course of courses" is like a for each. It basically defines course to hold one item of courses -->
-        </li>                                       
-    </ul>
+      
     `
 })
 export class CoursesComponent{

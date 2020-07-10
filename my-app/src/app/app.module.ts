194e0c60ab6463b2912ac00127cd573b8087ab2d
Ben Gardiner <bennygards@gmail.com>
2020-07-10 11:49:17 +1000

    creating a service manually - singleton pattern

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -5,17 +5,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses.component';
 import { CourseComponent } from './course/course.component';
+import { CoursesService } from './courses.service';
 
 @NgModule({
   declarations: [
     AppComponent, 
-    CoursesComponent, CourseComponent
+    CoursesComponent, 
+    CourseComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    CoursesService
+    //When you register a dependency as a provide in a module as we have done here, Angualr creates a single instance of this class for the entire module.
+    //so if we had 100 components and 50 of them need this CoursesService - still only one instance serves them all. (Singleton Pattern)
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- my-app/src/app/courses.component.ts
+++ my-app/src/app/courses.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CoursesService } from './courses.service';
 
 @Component({
     selector: 'courses',
@@ -18,6 +19,13 @@ export class CoursesComponent{
     title = "List of courses"
 
     courses;
-
     //logic for an HTTp serive - if you link to the service here it will "tightly couple" the component to a live HTTP endpoint - this could fuck the unit testing later on
+
+    constructor (service: CoursesService){
+       /// let service = new CoursesService; if we instatiated a new serive here like this - it tighly couples that to this for the future.. if anything changes down the track you would create multiple places to change the code. 
+        this.courses = service.getCourses();
+        ///instead we add the service as a paramater - when the component is created angular says this has a dependency called "courses Service"
+        //it then creates an isntance of courses service an passes it to CoursesComponent - therefore ay changes to the service class will immeaditaly be uptaken.
+
+    }
 }

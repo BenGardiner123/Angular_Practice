0c9b0c964309219ea5ae5075bd01493b81c91550
Ben Gardiner <bennygards@gmail.com>
2020-07-10 11:22:50 +1000

    creating a directive

--- my-app/src/app/courses.component.ts
+++ my-app/src/app/courses.component.ts
@@ -2,12 +2,23 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'courses',
-    template: '<h2>{{ getTitle() }}</h2>'
+    template: `                 
+    <!-- ////using the the backtick here allows us to spread the tempalte over multiple lines -->
+    <h2>{{ title }}</h2>
+    <ul>
+    <!-- when we use a directive like ngFor that manipulates the DOM by adding or removing an elemt- need to use an asterisk to annotate it -->
+        <li *ngFor="let course of courses">   
+              {{ course }}
+              <!-- "let course of courses" is like a for each. It basically defines course to hold one item of courses -->
+        </li>                                       
+    </ul>
+    
+    `
 })
 export class CoursesComponent{
     title = "List of courses"
 
-    getTitle() {
-        return this.title;
-    }
+    courses = ["course1", "course2", "course3"]
+
+    //logic for an HTTp serive - if you link to the service here it will "tightly couple" the component to a live HTTP endpoint - this could fuck the unit testing later on
 }
